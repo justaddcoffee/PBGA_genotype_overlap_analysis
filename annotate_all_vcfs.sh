0155f0a47428140ce9b5a0abc480b81950e58afb
Justin Reese <justaddcoffee+github@gmail.com>
2019-08-21 14:24:38 -0700

    initial commit

---
+++ annotate_all_vcfs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+export jar="/Users/jtr4v/IdeaProjects/PBGA/pbga-cli/target/pbga-cli-0.0.1-SNAPSHOT.jar"
+export yaml="/Users/jtr4v/IdeaProjects/PBGA/pbga-cli/src/main/resources/application.justin.yml"
+export java_cmd="java -jar $jar --config $yaml annotate-sv "
+# for file in `ls -1 data/*vcf.gz | grep -v cloudSV.PBOnly.HG00512.012317.ILL.genotyped.vcf | grep -v cloudSV.PBOnly.HG00513.012317.ILL.genotyped.vcf`
+for file in `ls data/cloudSV.PBOnly.HG00512.012317.ILL.genotyped.vcf.gz data/cloudSV.PBOnly.HG00513.012317.ILL.genotyped.vcf.gz`
+do
+    echo ===
+    echo analyzing $file
+    output_file=annotated/`basename $file`
+    export cmd="$java_cmd $file $output_file"
+    echo $cmd
+    $cmd    
+    echo ~~~
+done
